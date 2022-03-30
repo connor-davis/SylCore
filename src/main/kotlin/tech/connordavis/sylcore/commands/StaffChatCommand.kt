797a61a0e3378a23dc10f35460be473c9654e0a5
Connor Davis <cnnrproton@gmail.com>
2022-03-30 18:35:54 +0200

    Update staff chat command

--- src/main/kotlin/tech/connordavis/sylcore/commands/StaffChatCommand.kt
+++ src/main/kotlin/tech/connordavis/sylcore/commands/StaffChatCommand.kt
@@ -25,61 +25,8 @@ class StaffChatCommand :
         } else {
             if (sender is Player)
                     when (commandLabel) {
-                        // "createChannel" -> {
-                        //     val channel = args[0]
-                        //     val rank = permissionsManager.playerGroups(sender.name).get(0)
-
-                        //     if (channel.isNullOrEmpty()) {
-                        //         sender.from(Prefixes.STAFF_CHAT, "Created $rank chat channel.")
-
-                        //         staffChatManager.addStaffChatPlayer(rank, sender)
-
-                        //         sender.from(Prefixes.STAFF_CHAT, "Created $rank chat channel.")
-                        //     } else {
-                        //         sender.from(Prefixes.STAFF_CHAT, "Joining $rank chat channel.")
-
-                        //         staffChatManager.addStaffChatPlayer(channel, sender)
-
-                        //         sender.from(Prefixes.STAFF_CHAT, "Joined $rank chat channel")
-                        //     }
-                        // }
-                        // "removeChannel" -> {}
-                        "joinChannel" -> {
-                            val channel = args[0]
-                            val rank = permissionsManager.playerGroups(sender.name).get(0)
-
-                            if (channel.isNullOrEmpty()) {
-                                sender.from(Prefixes.STAFF_CHAT, "Joining $rank chat channel.")
-
-                                staffChatManager.addStaffChatPlayer(rank, sender)
-
-                                sender.from(Prefixes.STAFF_CHAT, "Joined $rank chat channel.")
-                            } else {
-                                sender.from(Prefixes.STAFF_CHAT, "Joining $rank chat channel.")
-
-                                staffChatManager.addStaffChatPlayer(channel, sender)
-
-                                sender.from(Prefixes.STAFF_CHAT, "Joined $rank chat channel")
-                            }
-                        }
-                        "leaveChannel" -> {
-                            val channel = args[0]
-                            val rank = permissionsManager.playerGroups(sender.name).get(0)
-
-                            if (channel.isNullOrEmpty()) {
-                                sender.from(Prefixes.STAFF_CHAT, "Leaving $rank chat channel.")
-
-                                staffChatManager.addStaffChatPlayer(rank, sender)
-
-                                sender.from(Prefixes.STAFF_CHAT, "Left $rank chat channel.")
-                            } else {
-                                sender.from(Prefixes.STAFF_CHAT, "Leaving $rank chat channel.")
-
-                                staffChatManager.addStaffChatPlayer(channel, sender)
-
-                                sender.from(Prefixes.STAFF_CHAT, "Left $rank chat channel")
-                            }
-                        }
+                        "staffchat" -> performSubcommands(sender, args)
+                        "sc" -> performSubcommands(sender, args)
                     }
             else {
                 sender.from(Prefixes.CORE, "Only players can use this command.")
@@ -88,6 +35,47 @@ class StaffChatCommand :
         }
 
         return false
+    }
+
+    fun performSubcommands(sender: Player, args: Array<String>) {
+        when (args[1]) {
+            "joinChannel" -> {
+                val channel = args[0]
+                val rank = permissionsManager.playerGroups(sender.name).get(0)
+
+                if (channel.isNullOrEmpty()) {
+                    sender.from(Prefixes.STAFF_CHAT, "Joining $rank chat channel.")
+
+                    staffChatManager.addStaffChatPlayer(rank, sender)
+
+                    sender.from(Prefixes.STAFF_CHAT, "Joined $rank chat channel.")
+                } else {
+                    sender.from(Prefixes.STAFF_CHAT, "Joining $rank chat channel.")
+
+                    staffChatManager.addStaffChatPlayer(channel, sender)
+
+                    sender.from(Prefixes.STAFF_CHAT, "Joined $rank chat channel")
+                }
+            }
+            "leaveChannel" -> {
+                val channel = args[0]
+                val rank = permissionsManager.playerGroups(sender.name).get(0)
+
+                if (channel.isNullOrEmpty()) {
+                    sender.from(Prefixes.STAFF_CHAT, "Leaving $rank chat channel.")
+
+                    staffChatManager.addStaffChatPlayer(rank, sender)
+
+                    sender.from(Prefixes.STAFF_CHAT, "Left $rank chat channel.")
+                } else {
+                    sender.from(Prefixes.STAFF_CHAT, "Leaving $rank chat channel.")
+
+                    staffChatManager.addStaffChatPlayer(channel, sender)
+
+                    sender.from(Prefixes.STAFF_CHAT, "Left $rank chat channel")
+                }
+            }
+        }
     }
 
     override fun onTabComplete(
